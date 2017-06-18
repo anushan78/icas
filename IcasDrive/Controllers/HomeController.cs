96b836aff19b3a525df5f9322016b2f3c7f527e3
sherpauat\anushan <anushan@sherpasystems.com.au>
2017-06-18 23:32:29 +1000

    First cut changes for the get files method with batch request

--- IcasDrive/Controllers/HomeController.cs
+++ IcasDrive/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Google.Apis.Auth.OAuth2.Mvc;
 using Google.Apis.Drive.v2;
 using Google.Apis.Drive.v2.Data;
+using Google.Apis.Requests;
 using Google.Apis.Services;
 using IcasDrive.Core;
 using IcasDrive.Models;
@@ -80,6 +81,60 @@ namespace IcasDrive.Controllers
             {
                 TempData.Add("examViewModel", model);
                 TempData.Add("isInitialPost", true);
+                return new RedirectResult(result.RedirectUri);
+            }
+        }
+
+        public ActionResult SelectPapers()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> SelectPapersAsync(CancellationToken cancellationToken, SubjectViewModel model)
+        {
+            var result = await new AuthorizationCodeMvcApp(this, new AppFlowMetadata()).AuthorizeAsync(cancellationToken);
+
+            if (result.Credential != null)
+            {
+                var service = new DriveService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = result.Credential,
+                    ApplicationName = ApplicationName
+                });
+
+                try
+                {
+                    var request = new BatchRequest(service);
+                    string[] fileIds = new string[] { "0ByOpc19oJzbjNGstb2xtdDhJeGc", "0ByOpc19oJzbjZzY4a1hub2ZRRzg" };
+                    List<string> fileUrls = new List<string>();
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        request.Queue<File>(service.Files.Get(fileIds[i]),
+                            (file, error, x, message) =>
+                            {
+                                if (error != null)
+                                {
+
+                                }
+                                else
+                                {
+                                    fileUrls.Add(file.WebContentLink);
+                                }
+                            });
+                    }
+
+                    await request.ExecuteAsync();
+                }
+                catch (Exception ex)
+                {
+                    // Todo: Log errors and show friendly error
+                }
+
+                return View("SelectPapers");
+            }
+            else
+            {
                 return new RedirectResult(result.RedirectUri);
             }
         }
