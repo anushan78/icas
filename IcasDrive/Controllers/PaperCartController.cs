b6e49d630e3357da8aa8b8af9aa3927cef6ab272
Anushan Tennakoon <anushan2003@yahoo.com>
2017-06-19 17:23:20 +1000

    Paper Cart initial checkin

---
+++ IcasDrive/Controllers/PaperCartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IcasDrive.Controllers
+{
+    public class PaperCartController : Controller
+    {
+        // GET: PaperCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IcasDrive/Models/PaperCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IcasDrive.Models
+{
+    public class PaperCartViewModel
+    {
+    }
+}
